c8c0ac41620ff646123fcf581ecbac1393859c4e
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-06-05 11:17:13 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 game 'rdr3'
 lua54 'yes'
-
+version '1.0'
 -- Replace YOURNAME with your user/development name.
 author 'YOURNAME'
 
